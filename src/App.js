370a10161173b9b32014f9d3b99c37bb929e4431
vermanivi <vermanivish@gmail.com>
2021-04-01 20:22:18 +0530

    Header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from "Components/header/Header";
+import Footer from "Components/footer/Footer"
+
 
 function App() {
   return (
     <div className = "App" >
-
+      <Header />
+     
         </div>
     );
 }
