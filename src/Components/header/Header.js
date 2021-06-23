a8779d274f674c633794793bc0f0f0e0f712a065
vermanivi <vermanivish@gmail.com>
2021-06-23 15:59:24 +0530

    Components

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
-const Header = () =>
-{
-    return (
-        <div className="header-wrapper">
-            <div className="container">
-                <div className="header-content">
-                    <div className="logo">Nivish-Blog</div>
-                    <ul className="nav-list">
-                        <li>
-                            About Us
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>);
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+const Header = (props) => {
+  const getActionButtons = () => {
+    if (!props.isAuth) return null;
+
+    const btn = props.location.pathname.includes("/home") ? (
+      <button onClick={() => props.toggleAdd(true)}>Add</button>
+    ) : null;
+
+    return btn;
+  };
+
+  return (
+    <div className="header-wrapper">
+      <div className="container">
+        <div className="header-content">
+          <div className="logo">DevBlog</div>
+          <ul className="nav-list">
+            {getActionButtons()}
+            <li>About Us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default Header;
+
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.isAuth,
+  };
+};
+
+export default connect(mapStateToProps, null)(withRouter(Header));
